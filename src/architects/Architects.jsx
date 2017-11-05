13998571c9b9becb8b4896c2deecbbd47d00573f
Jeff Roush <jeff@jeffroush.com>
2017-11-05 07:59:00 -0800

    Begin to add architects

---
+++ src/architects/Architects.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+
+const Architects = () => (
+  <div>
+    <p>Hi!</p>
+  </div>
+)
+
+export default Architects
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Architects from './architects/Architects';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Architects />, document.getElementById('root'));
 registerServiceWorker();
