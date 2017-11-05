6de6c33e7072d3d78e4ea3121e9f6169ba60f456
Jeff Roush <jeff@jeffroush.com>
2017-11-05 08:27:34 -0800

    Show list of architects

--- src/architects/Architects.jsx
+++ src/architects/Architects.jsx
@@ -8,11 +8,39 @@ import {
 class Architects extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {
+      architects: []
+    }
   }
+
+  componentWillReceiveProps (nextProps) {
+    this.getArchitects()
+  }
+
+  componentDidMount() {
+    this.getArchitects()
+  }
+
+  getArchitects () {
+    var url = 'http://localhost:3001/architects.json'
+    fetch(url).then((response) => {
+      return response.json()
+    }).then((json) => {
+      this.setState({
+        architects: json.data.map(architect => ({name: architect.attributes.name, id: architect.id}) )
+      })
+    })
+  }
+
   render() {
     return (
       <div>
         <p>Hi!</p>
+        {
+          this.state.architects.map(function(architect) {
+            return <li>{architect.name}</li>
+          })
+        }
       </div>
     )
   }
